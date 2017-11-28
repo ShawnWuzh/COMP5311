08acf8a2919675ffa27d8fb1d525d30f38deb31e
Zac Wu <wuzh@ZAC-MacBook-Pro.local>
2017-11-28 22:53:04 +0800

    hello

--- task2.py
+++ task2.py
@@ -36,24 +36,16 @@ class LinearTopo(Topo):
         # wire up the receiver to the first swtich
         self.addLink(receiver,switches[0])
 
-def start_tcpprobe(args):
-    os.system("rmmod tcp_probe 1>/dev/null 2>&1; modprobe tcp_probe")
+def start_tcpprobe(args,port):
+    os.system("rmmod tcp_probe 1>/dev/null 2>&1; modprobe tcp_probe port={}".format(str(port)))
     Popen("cat /proc/net/tcpprobe > %s/tcp_probe.txt" % args.folder, shell=True)
 
 def stop_tcpprobe():
     os.system("killall -9 cat; rmmod tcp_probe")
 
-def monitor_devs_ng(fname, interval_sec=0.01):
-    """Uses bwm-ng tool to collect iface tx rate stats.  Very reliable."""
-    cmd = ("sleep 1; bwm-ng -t %s -o csv "
-           "-u bits -T rate -C ',' > %s" %
-           (interval_sec * 1000, fname))
-    Popen(cmd, shell=True).wait()
-
-
 def check_prereqs():
     "Check for necessary programs"
-    prereqs = ['telnet', 'bwm-ng', 'iperf', 'ping']
+    prereqs = ['telnet', 'iperf', 'ping']
     # bwm-ng if for testing bandwidth
     for p in prereqs:
         if not quietRun('which ' + p):
@@ -81,14 +73,11 @@ def run_linear_topology_test(net,args):
     The function is for testing the linear Topology
     '''
 
-    # The monitot is for monitoring the throughput of the network
     n = args.n
-    monitor = Process(target=monitor_devs_ng, args=('%s/bwm.txt' % args.folder, 1.0))
-    monitor.start()
-    start_tcpprobe(args)
+    port = 2048
+    start_tcpprobe(args，port)
     recvr = net.getNodeByName('receiver')
     # the port_num is the port number server waiting for
-    port = 2048
     recvr.cmd('iperf -s -p', port, '> %s/iperf_server.txt' % args.folder, '&')
     h = [] # Python list of clients
     for i in range(n):
@@ -98,16 +87,13 @@ def run_linear_topology_test(net,args):
      # send iperf cmd to all hosts
     for i in range(n):
         node_name = 'h%s' % (i+1)
-        h[i].sendCmd('iperf -c %s -p %s -i 1 -yc > %s/iperf_%s.txt' % (recvr.IP(), port, args.folder, node_name))
+        h[i].sendCmd('iperf -c %s -p %s -i 1  > %s/iperf_%s.txt' % (recvr.IP(), port, args.folder, node_name))
 
     # wait for commands to finish
     iperf_results = {}
     for i in range(n):
         iperf_results[h[i].name] = h[i].waitOutput()
     recvr.cmd('kill %iperf')
-
-    # terminate monitors
-    monitor.terminate()
     stop_tcpprobe()
 
 if __name__=='__main__':
@@ -115,8 +101,8 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Linear Topology Test')
     parser.add_argument('--n','-n',required=True,type=int,help='the number of hosts')
     parser.add_argument('--bandwidth','-b',default=10,type=int,help='the bandwidth of the link')
-    parser.add_argument('--delay','-d',default='10ms',type=str,help='the delay in the link')
-    parser.add_argument('--loss','-l',default=10,type=int,help='the loss rate in the link')
+    parser.add_argument('--delay','-d',default='0ms',type=str,help='the delay in the link')
+    parser.add_argument('--loss','-l',default=0,type=int,help='the loss rate in the link')
     parser.add_argument('--folder', '-f',help="folder to store outputs",default="results")
     args=parser.parse_args()
     if not os.path.exists(args.folder):
